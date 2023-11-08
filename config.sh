6bd4a63d100727f0e009ef6e2da791ee8e4ecb8b
Davi N. de Azevedo <70330402+DN4Davi@users.noreply.github.com>
2023-11-07 21:08:34 -0300

    add python build dependencies

--- config.sh
+++ config.sh
@@ -27,6 +27,9 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 tail -n 3 .bashrc >> .zshrc
+sudo apt update; sudo apt install build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev curl \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
 
 
 # Install Docker Engine
